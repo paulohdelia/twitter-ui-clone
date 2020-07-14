eec5383d95509b414396fc04b0fdfdbd3f1c58a0
paulodelia <paulohdelia@gmail.com>
2020-07-14 12:26:03 -0300

    feat: add Layout container and wrapper

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const Layout: React.FC = () => {
+  return (
+    <>
+      <Container>
+        <Wrapper>
+          {/* <MenuBar /> */}
+          <Main />
+          {/* <SideBar /> */}
+        </Wrapper>
+      </Container>
+    </>
+  );
+};
+
+export default Layout;
